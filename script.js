f4c2c3ee9aa0b48d04873fa2c055ed05a0a9ed8c
NETCRAFT\nataliai <nataliai@netcraft.co.il>
2018-10-17 11:03:58 +0300

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+(function() {
+    var follower, init,  printout, timer;
+
+    follower = document.getElementById('follower');
+
+    printout = document.getElementById('printout');
+
+    function mouseX(event) {
+        return event.clientX;
+    };
+
+    function mouseY(event) {
+        return event.clientY;
+    };
+
+    function positionElement(event) {
+        var mouse;
+        mouse = {
+            x: mouseX(event),
+            y: mouseY(event)
+        };
+        follower.style.clipPath = 'circle(150px at ' + mouse.x + 'px ' + mouse.y + 'px)';
+
+        // return follower.style.left = mouse.x + 'px';
+    };
+
+    timer = false;
+
+    window.onmousemove = function(e) {
+        console.log();
+        var _event;
+        _event = event;
+        return timer = setTimeout(function() {
+            return positionElement(_event);
+    }, 1);
+    };
+
+}).call(this);
